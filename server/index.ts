20970150031f5d5572a3b4b40a5c004db6901168
Nency Patel <ndpatelsokhada2006@gmail.com>
2026-03-23 20:16:03 +0530

    Add user authentication (register and login) with MongoDB Atlas

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL!)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api", authRoutes);
+
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("Server running");
 });
 
 app.listen(5000, () => {
